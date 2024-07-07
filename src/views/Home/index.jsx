a285bc5e3bafa7f91102927d20e5bf76f9391264
Cristal Flores <cristalflores.bo@gmail.com>
2024-07-06 20:10:48 -0400

    Usando fetch en Pantalla de Detalle

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -2,13 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import Navbar from "../../components/Navbar";
 import Events from "../../components/Events";
 import useEventsData from "../../hooks/userEventsData";
+import ReactPaginate from "react-paginate";
+import styles from "./Home.module.css";
 
 const Home = () => {
-    const { events, isLoading, error, fetchEvents } = useEventsData();
+    const { events, isLoading, error, fetchEvents, page } = useEventsData();
     const [searchTerm, setSearchTerm] = useState("");
     const containerRef = useRef();
 
-    useEffect(() => {    
+    useEffect(() => {
         fetchEvents();
     }, []);
 
@@ -18,11 +20,45 @@ const Home = () => {
         fetchEvents(`&keyword=${term}`);
     };
 
+    const handlePageClick = ({ selected }) => {
+        fetchEvents(`&keyword=${searchTerm}&page=${selected}`);
+    };
+
+    const renderEvents = () => {
+        if (isLoading) {
+            return <p>Cargando resultados...</p>;
+        }
+
+        if (error) {
+            return <p>Ha ocurrido un error</p>;
+        }
+
+        return (
+            <div>
+                <Events searchTerm={searchTerm} events={events} />
+                <ReactPaginate
+                    className={styles.pagination}
+                    nextClassName={styles.next}
+                    previousClassName={styles.previous}
+                    pageClassName={styles.page}
+                    activeClassName={styles.activePage}
+                    disabledClassName={styles.disabledPage}
+                    breakLabel="..."
+                    nextLabel=">"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={page.totalPages}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+                />
+            </div>
+        );
+    };
+
     return (
         <>
             <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
-            {isLoading ? <p>Cargando resultados...</p> : <Events searchTerm={searchTerm} events={events} />}
-            {!!error && <p>Ha ocurrido un error</p>}
+            {renderEvents()}
         </>
     );
 };
