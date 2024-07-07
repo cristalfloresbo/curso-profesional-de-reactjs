a7b0f943c82387ee17df30035df3e9a26ed81686
Cristal Flores <cristalflores.bo@gmail.com>
2024-07-07 00:10:34 -0400

    Estilizando tabs

--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import styles from "./Profile.module.css";
 
 const Profile = () => {
@@ -10,20 +10,27 @@ const Profile = () => {
     };
     return (
         <div>
-            <div>
+            <Link to="/" className={styles.homeLink}>Inicio</Link>
+            <div className={styles.tabsContainer}>
                 <span
                     className={`${
                         pathname.includes("my-info") ? styles.active : ""
-                    }`}
+                    } ${styles.tab}`}
                     onClick={() => handleTabClick("my-info")}
+                    style={{
+                        marginRight: "8pxs",
+                    }}
                 >
                     Mi información
                 </span>
                 <span
                     className={`${
                         pathname.includes("liked-events") ? styles.active : ""
-                    }`}
+                    } ${styles.tab}`}
                     onClick={() => handleTabClick("liked-events")}
+                    style={{
+                        marginRight: "8pxs",
+                    }}
                 >
                     Eventos Favoritos
                 </span>
