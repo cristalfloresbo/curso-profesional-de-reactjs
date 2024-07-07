bdf831c81a3f98bc099646541bf225868e81641a
Cristal Flores <cristalflores.bo@gmail.com>
2024-07-06 21:15:06 -0400

    Agregando informacion al detalle del evento

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,10 +1,77 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
 const Detail = () => {
     const { eventId } = useParams();
+    const [eventData, setEventData] = useState({});
+    const [error, setError] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
-    console.log(eventId);
-    return <div>Detail</div>;
+    useEffect(() => {
+        const fetchEventData = async () => {
+            try {
+                const response = await fetch(
+                    `https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=SnXyjG5sBVR6yyvkUxokXNl7wUGHLJiH`
+                );
+                const data = await response.json();
+                setEventData(data);
+                console.log(data);
+                setIsLoading(false);
+            } catch (err) {
+                setEventData({});
+                setError(err);
+                console.log(err);
+                setIsLoading(false);
+            }
+        };
+        fetchEventData();
+    }, []);
+
+    if (isLoading && Object.keys(eventData).length === 0) {
+        return <p>Cargando...</p>;
+    }
+
+    if (Object.keys(error) > 0) {
+        return <p>Ha ocurrido un error</p>;
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.mainInfoContainer}>
+                <img
+                    src={eventData.images?.[0].url}
+                    className={styles.eventImage}
+                    alt={eventData.name}
+                />
+                <h4 className={styles.eventName}>{eventData.name}</h4>
+                <p className={styles.infoParagraph}>{eventData.info}</p>
+                {eventData.dates?.start.dateTime ? (
+                    <p className={styles.dateParagraph}>
+                        {format(
+                            new Date(eventData.dates?.start.dateTime),
+                            "d LLLL yyyy H:mm",
+                            { locale: es }
+                        )}
+                        hrs
+                    </p>
+                ) : null}
+            </div>
+            <div className={styles.seatInfoContainer}>
+                <h6 className={styles.seatMapTitle}>Mapa del evento</h6>
+                <img src={eventData.seatmap?.staticUrl} alt="Seatmap event" />
+                <p className={styles.pleaseLegend}>{eventData.pleaseNote}</p>
+                <p className={styles.priceRangesLegend}>
+                    {eventData.priceRanges?.[0].min} -{" "}
+                    {eventData.priceRanges?.[0].max}{" "}
+                    {eventData.priceRanges?.[0].currency}
+                </p>
+            </div>
+            <a href={eventData.url}>Ir por tus boletos</a>
+        </div>
+    );
 };
 
 export default Detail;
